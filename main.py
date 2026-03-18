47ccb4b2c47118a57cc9250ac455e95a6d97075f
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-18 13:14:32 -0400

    d Snake class and added a second main to test OOP version

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+# OOP Version
+from turtle import Turtle, Screen
+from snake import Snake
+# Initialize Screen Object
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game Nokia")
+snake = Snake()
+snake.create_snake()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
