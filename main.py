1bc53eafbae0bd145ef23a30a5e83eb61e3f2375
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-18 16:13:23 -0400

    add snake class methods to turn east, north, west and south with logic to stop it from turning back on its head ex. heading is east can't turn west/ heading is north can't turn south and vice versa

--- main.py
+++ main.py
@@ -11,11 +11,19 @@ screen.tracer(0)
 
 # Initialize Snake Object
 snake = Snake()
+# # Listen for key events
+screen.listen()
+screen.onkey(snake.turn_east, "Right")
+screen.onkey(snake.turn_north, "Up")
+screen.onkey(snake.turn_west, "Left")
+screen.onkey(snake.turn_south, "Down")
+# Initialize Game
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
     snake.auto_move()
+
 
 
 
--- snake.py
+++ snake.py
@@ -1,10 +1,15 @@
 from turtle import Turtle
-STARTING_POSITIONS = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+STARTING_POSITIONS = [(0,0), (-20,0), (-40, 0)]
 DEFAULT_MOVE_DISTANCE = 20
+EAST = 0
+NORTH = 90
+WEST = 180
+SOUTH = 270
 class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.head = self.snake[0]
     def create_snake(self):
         for position in STARTING_POSITIONS:
             segment = Turtle()
@@ -18,4 +23,20 @@ class Snake:
             new_x = self.snake[piece_num - 1].xcor()
             new_y = self.snake[piece_num - 1].ycor()
             self.snake[piece_num].goto(new_x, new_y)
-        self.snake[0].forward(DEFAULT_MOVE_DISTANCE)
+        self.head.forward(DEFAULT_MOVE_DISTANCE)
+    def turn_east(self):
+        if self.head.heading() !=WEST:
+            self.head.setheading(EAST)
+            print("turn east")
+    def turn_north(self):
+        if self.head.heading() != SOUTH:
+            self.head.setheading(NORTH)
+            print("turn north")
+    def turn_west(self):
+        if self.head.heading() != EAST:
+            self.head.setheading(WEST)
+            print("turn west")
+    def turn_south(self):
+        if self.head.heading() != NORTH:
+            self.head.setheading(SOUTH)
+            print("turn south")
