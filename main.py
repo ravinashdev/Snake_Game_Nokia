496e718da76b04e65cef1132746bc7caba75e374
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-18 22:44:13 -0400

    create score_board class object methods increase_score() and write_score() to display current score when food object collision is detected with snake object

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
+from score_board import ScoreBoard
 # Initialize Screen Object
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -14,6 +15,8 @@ screen.tracer(0)
 snake = Snake()
 # Initialize Food Object
 food = Food()
+# Initialize ScoreBoard Object
+score_board = ScoreBoard()
 # # Listen for key events to invoke methods to change direction
 screen.listen()
 screen.onkey(snake.turn_east, "Right")
@@ -25,10 +28,19 @@ game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
+    # Initiate snake.auto_move method so snake is not stationary
     snake.auto_move()
+    # Write the score_board.write_score() method with the initial conditions
+    score_board.write_score()
+    # Detect the collision of snake object with food object by measuring its pixel distance
     if snake.head.distance(food) <= 15:
         print("Got it")
+        # When collision is detected:
+        # Create a new instance of the food object food.new_food() and give it a random position
         food.new_food()
+        # Update the score_board object methods score_board.increase_score() and score_board.write_score() so they can display on the screen object
+        score_board.increase_score()
+        score_board.write_score()
 
 
 
--- score_board.py
+++ score_board.py
@@ -1,3 +1,23 @@
-class ScoreBoard:
+from turtle import Turtle
+
+from main import score_board
+
+
+class ScoreBoard(Turtle):
     def __init__(self):
-        pass
+        super().__init__()
+        # Set the initial conditions of the score_board Object
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.setposition(0,270)
+        self.hideturtle()
+    def write_score(self):
+        # Create a message f string as write method can't use an f string
+        message = f"Score Board: {self.score}"
+        self.write(message, move=False, align="center", font=("Courier", 16, "bold"))
+    def increase_score(self):
+        # Increase the score everytime a collision with Food class is detected in main.py
+        self.score += 1
+        # Clear the screen so new score can get written when it's incremented by 1'
+        self.clear()
